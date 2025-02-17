f14199b3fa6eebcb4f116359e6b0dc8aec0bd0ae
anon <anon@anon.anon>
2025-02-17 16:36:44 +0100

    init

---
+++ test.h
@@ -0,0 +1,22 @@
+/* This is an obnoctious copyright notice, clearly written by a blind man.
+ */
+#ifndef TEST_H
+#define TEST_H
+/* This comment describes the functions below,
+    in long form.
+*/
+void foo(void);
+void bar(void);
+
+// this function is exceptional somehow
+void fizzbuzz(int i);
+
+
+
+// this is some real strange newlininging
+
+
+void wicked(const char * const str, const size_t n);
+
+
+#endif
